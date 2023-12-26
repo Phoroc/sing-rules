d214c46b9557afbd4d27db913486858827873dd7
Phoroc <19701027+Phoroc@users.noreply.github.com>
2023-12-26 03:28:15 +0000

    Add files via upload

---
+++ generate_rule_set.py
@@ -0,0 +1,58 @@
+import math
+import re
+import requests
+import json
+import os
+
+hagezi_blocklist = [
+    "https://raw.githubusercontent.com/hagezi/dns-blocklists/main/wildcard/light-onlydomains.txt",
+    "https://raw.githubusercontent.com/hagezi/dns-blocklists/main/wildcard/multi-onlydomains.txt",
+    "https://raw.githubusercontent.com/hagezi/dns-blocklists/main/wildcard/pro-onlydomains.txt",
+    "https://raw.githubusercontent.com/hagezi/dns-blocklists/main/wildcard/pro.plus-onlydomains.txt",
+    "https://raw.githubusercontent.com/hagezi/dns-blocklists/main/wildcard/ultimate-onlydomains.txt",
+    "https://raw.githubusercontent.com/hagezi/dns-blocklists/main/wildcard/tif-onlydomains.txt"
+]
+
+output_dir = "./rule-set"
+
+
+def convert_domains(url: str) -> str:
+    r = requests.get(url)
+    domain_list = []
+    if r.status_code == 200:
+        lines = r.text.splitlines()
+        for line in lines:
+            if not line.startswith("#"):
+                domain_list.append(line)
+    result = {
+        "version": 1,
+        "rules": [
+            {
+                "domain_suffix": []
+            }
+        ]
+    }
+    result["rules"][0]["domain_suffix"] = domain_list
+    filepath = os.path.join(output_dir, url.split("/")[-1] + ".json")
+    with open(filepath, "w") as f:
+        f.write(json.dumps(result, indent=4))
+    return filepath
+
+
+def main():
+    files = []
+    os.mkdir(output_dir)
+    for url in hagezi_blocklist:
+        filepath = convert_domains(url)
+        files.append(filepath)
+    print("rule-set source generated:")
+    for filepath in files:
+        print(filepath)
+    for filepath in files:
+        srs_path = filepath.replace(".json", ".srs")
+        os.system("sing-box rule-set compile --output " +
+                  srs_path + " " + filepath)
+
+
+if __name__ == "__main__":
+    main()
