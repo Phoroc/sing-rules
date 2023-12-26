db29a6e84880c0c97e0c94b60aec2d79497537bf
Phoroc <19701027+Phoroc@users.noreply.github.com>
2023-12-26 07:08:58 +0000

    Update generate_rule_set.py

--- generate_rule_set.py
+++ generate_rule_set.py
@@ -4,26 +4,33 @@ import requests
 import json
 import os
 
-hagezi_blocklist = [
-    "https://raw.githubusercontent.com/hagezi/dns-blocklists/main/wildcard/light-onlydomains.txt",
-    "https://raw.githubusercontent.com/hagezi/dns-blocklists/main/wildcard/multi-onlydomains.txt",
-    "https://raw.githubusercontent.com/hagezi/dns-blocklists/main/wildcard/pro-onlydomains.txt",
-    "https://raw.githubusercontent.com/hagezi/dns-blocklists/main/wildcard/pro.plus-onlydomains.txt",
-    "https://raw.githubusercontent.com/hagezi/dns-blocklists/main/wildcard/ultimate-onlydomains.txt",
-    "https://raw.githubusercontent.com/hagezi/dns-blocklists/main/wildcard/tif-onlydomains.txt"
+lists_hagezi = [
+    ["hagezi-light", "https://raw.githubusercontent.com/hagezi/dns-blocklists/main/wildcard/light-onlydomains.txt"],
+    ["hagezi-normal", "https://raw.githubusercontent.com/hagezi/dns-blocklists/main/wildcard/multi-onlydomains.txt"],
+    ["hagezi-pro", "https://raw.githubusercontent.com/hagezi/dns-blocklists/main/wildcard/pro-onlydomains.txt"],
+    ["hagezi-proplus", "https://raw.githubusercontent.com/hagezi/dns-blocklists/main/wildcard/pro.plus-onlydomains.txt"],
+    ["hagezi-ultimate", "https://raw.githubusercontent.com/hagezi/dns-blocklists/main/wildcard/ultimate-onlydomains.txt"],
+    ["hagezi-tif", "https://raw.githubusercontent.com/hagezi/dns-blocklists/main/wildcard/tif-onlydomains.txt"]
+]
+
+lists_1hosts = [
+    ["1hosts-mini", "https://raw.githubusercontent.com/badmojr/1Hosts/master/mini/domains.wildcards"],
+    ["1hosts-lite", "https://raw.githubusercontent.com/badmojr/1Hosts/master/Lite/domains.wildcards"],
+    ["1hosts-pro", "https://raw.githubusercontent.com/badmojr/1Hosts/master/Pro/domains.wildcards"],
+    ["1hosts-xtra", "https://raw.githubusercontent.com/badmojr/1Hosts/master/Xtra/domains.wildcards"]
 ]
 
 output_dir = "./rule-set"
 
 
-def convert_domains(url: str) -> str:
-    r = requests.get(url)
+def convert_domains(list_info: list) -> str:
+    r = requests.get(list_info[1])
     domain_list = []
     domain_suffix_list = []
     if r.status_code == 200:
         lines = r.text.splitlines()
         for line in lines:
-            if not line.startswith("#"):
+            if not line.startswith("#") and line.strip():
                 domain_list.append(line)
                 domain_suffix_list.append("." + line)
     result = {
@@ -37,17 +44,20 @@ def convert_domains(url: str) -> str:
     }
     result["rules"][0]["domain"] = domain_list
     result["rules"][0]["domain_suffix"] = domain_suffix_list
-    filepath = os.path.join(output_dir, url.split("/")[-1] + ".json")
+    filepath = os.path.join(output_dir, list_info[0] + ".json")
     with open(filepath, "w") as f:
-        f.write(json.dumps(result, indent=4))
+        f.write(json.dumps(result, indent=2))
     return filepath
 
 
 def main():
     files = []
     os.mkdir(output_dir)
-    for url in hagezi_blocklist:
-        filepath = convert_domains(url)
+    for ls in lists_hagezi:
+        filepath = convert_domains(ls)
+        files.append(filepath)
+    for ls in lists_1hosts:
+        filepath = convert_domains(ls)
         files.append(filepath)
     print("rule-set source generated:")
     for filepath in files:
